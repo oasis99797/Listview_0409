5b9fdce47884e1a7111cd7147a047b07a4a51a60
oasis99797 <oasis99797@gmail.com>
2020-04-13 20:52:37 +0900

    실데이터 리스트에 반영

--- app/src/main/java/com/bklee/listview_0409/adapters/RoomAdapter.kt
+++ app/src/main/java/com/bklee/listview_0409/adapters/RoomAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.bklee.listview_0409.datas.Room
 import com.bklee.listview_0409.R
 
@@ -23,6 +24,42 @@ class RoomAdapter(context: Context, resId:Int, list: ArrayList<Room>) : ArrayAda
         val row = tempRow!!
 
 //        row변수를 활용해서 각줄에 맞는 데이터 가공.
+
+//        XML에 있는 뷰들을 변수로 담기. findViewById
+        val priceTxt = row.findViewById<TextView>(R.id.priceTxt)
+        val addressAndFloorTxt = row.findViewById<TextView>(R.id.addressAndFloorTxt)
+        val descTxt = row.findViewById<TextView>(R.id.descTxt)
+
+//        근거 데이터 변수 추출. => 각 위치에 맞는 데이터를 mList에서 가져오기
+        val data = mList.get(position)
+
+//        가격이 1만 이상 ? "?억 ?" 식, 그 이하 "?"
+        if (data.price >= 10000) {
+            priceTxt.text = "${data.price / 10000}억 ${String.format("%,d", data.price % 10000)}"
+        }
+        else {
+            priceTxt.text = String.format("%,d", data.price)
+        }
+
+
+//        주소와 층수 => "주소, 층수(상황마다다른값)"
+
+        var floorStr = ""
+
+        if (data.floor == 0) {
+            floorStr = "반지하"
+        }
+        else if (data.floor > 0) {
+            floorStr = "${data.floor}층"
+        }
+        else {
+            floorStr = "지하 ${-data.floor}층"
+        }
+
+        addressAndFloorTxt.text = "${data.address}, ${floorStr}"
+
+//        설명은 그냥 있는 그대로 대입
+        descTxt.text = data.desc
 
         return row
 
--- app/src/main/java/com/bklee/listview_0409/datas/Room.kt
+++ app/src/main/java/com/bklee/listview_0409/datas/Room.kt
@@ -1,8 +1,8 @@
 package com.bklee.listview_0409.datas
 
-class Room {
-    var prices = 0
-    var address = "?구 ?동"
-    var floor = 1 // 1 ~ 층수, 0층: 반지하, -2: 지하 2층
-    var description = "방에 대한 설명"
+class Room(price:Int, address:String, floor:Int, desc:String) {
+    var price = price
+    var address = address
+    var floor = floor // 1~ 층수. 0층 : 반지하, -2.. 지하2층
+    var desc = desc
 }
